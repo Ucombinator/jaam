2626cb5d2a6b6af1b00b6d24637081cd8646bc48
Michael D. Adams <http://michaeldadams.org/>
2015-10-15 12:12:03 -0600

    Update Fibonacci

--- to-analyze/Fibonacci.java
+++ to-analyze/Fibonacci.java
@@ -1,6 +1,6 @@
 class Fibonacci{
   public static void main(String[] args){
-    //fibonacci(0);
+    fibonacci(0);
   }
   public static int fibonacci(int n) {
     if(n<=1)
@@ -9,4 +9,3 @@ class Fibonacci{
       return fibonacci(n-1)+fibonacci(n-2);
   }
 }
-
