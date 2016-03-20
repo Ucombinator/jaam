397501bc1d86b97c3ddf701aabc1113856513577
Guannan Wei <kiss.kraks@gmail.com>
2016-03-20 00:50:41 -0600

    fix CmpExpr

--- src/main/scala/org/ucombinator/jaam/Main.scala
+++ src/main/scala/org/ucombinator/jaam/Main.scala
@@ -525,7 +525,7 @@ case class State(val stmt : Stmt,
       case v : NegExpr => D.atomicTop
       case v : BinopExpr =>
         v match {
-          case (_ : EqExpr) | (_ : NeExpr) | (_ : GeExpr) | (_ : GtExpr) | (_ : LeExpr) | (_ : LtExpr) =>
+          case (_ : EqExpr) | (_ : NeExpr) | (_ : GeExpr) | (_ : GtExpr) | (_ : LeExpr) | (_ : LtExpr) | (_ : CmpExpr) =>
             eval(v.getOp1)
             eval(v.getOp2)
             D.atomicTop
