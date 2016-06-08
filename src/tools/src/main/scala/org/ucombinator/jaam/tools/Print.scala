8e28ef7c4e13780b8a45d45ea485baccb5b9a48f
Pierce Darragh <pierce.darragh@gmail.com>
2016-06-08 14:49:06 -0600

    Updates to print
    * no deduplication
    * fixed edge ID printing
    * added "node-" and "edge-" prefixes to make output easily greppable
    * removed `class` and `method` from node output; now just uses soot equivalents alone

--- src/tools/src/main/scala/org/ucombinator/jaam/tools/Print.scala
+++ src/tools/src/main/scala/org/ucombinator/jaam/tools/Print.scala
@@ -6,10 +6,6 @@ import scala.collection.mutable
 import org.ucombinator.jaam.serializer._
 
 object Print {
-  var currentID : Int = -1
-  val uniqueNodes = mutable.Set.empty[Id[Node]]
-  val uniqueEdges = mutable.Set.empty[Id[Edge]]
-
   def printFile(jaamFile : String) = {
     val stream = new FileInputStream(jaamFile)
     val pi = new PacketInput(stream)
@@ -47,52 +43,41 @@ object Print {
   }
 
   def printNode(node : Node) = {
-    if (! uniqueNodes.contains(node.id)) {
-      uniqueNodes.add(node.id)
-
-      node match {
-        case n: State => printState(n)
-        case n: ErrorState => printErrorState(n)
-      }
+    node match {
+      case n: State => printState(n)
+      case n: ErrorState => printErrorState(n)
     }
   }
 
   def printState(state : State) = {
-    currentID = state.id.id
-    printIndentedLine(0, "State")
-    printIndentedLine(1, "id: " + state.id.id)
-    printIndentedLine(1, "index: " + state.stmt.index)
-    printIndentedLine(1, "class: " + state.stmt.method.getDeclaringClass.toString)
-    printIndentedLine(1, "method: " + state.stmt.method.getName)
-    printIndentedLine(2, "returns: " + state.stmt.method.getReturnType.toString)
+    val identifier = "node-" + state.id.id
+    printIndentedLine(identifier, 0, "State")
+    printIndentedLine(identifier, 1, "id: " + state.id.id)
+    printIndentedLine(identifier, 1, "index: " + state.stmt.index)
+    printIndentedLine(identifier, 1, "sootMethod: " + state.stmt.method.toString)
+    printIndentedLine(identifier, 1, "sootStmt: " + state.stmt.stmt.toString())
+    printIndentedLine(identifier, 2, "returns: " + state.stmt.method.getReturnType.toString)
     if (state.stmt.method.getParameterCount == 0) {
-      printIndentedLine(2, "takes: <none>")
+      printIndentedLine(identifier, 2, "takes: <none>")
     } else {
-      printIndentedLine(2, "takes: " + state.stmt.method.getParameterTypes.toArray.mkString(", "))
+      printIndentedLine(identifier, 2, "takes: " + state.stmt.method.getParameterTypes.toArray.mkString(", "))
     }
-    printIndentedLine(1, "soot:")
-    printIndentedLine(2, "sootMethod: " + state.stmt.method.toString)
-    printIndentedLine(2, "sootStmt: " + state.stmt.stmt.toString())
   }
 
   def printErrorState(errorState : ErrorState) = {
-    currentID = errorState.id.id
-    printIndentedLine(0, "ErrorState")
-    printIndentedLine(1, "id: " + errorState.id.id)
+    val identifier = "node-" + errorState.id.id
+    printIndentedLine(identifier, 0, "ErrorState")
+    printIndentedLine(identifier, 1, "id: " + errorState.id.id)
   }
 
   def printEdge(edge : Edge) = {
-    if (! uniqueEdges.contains(edge.id)) {
-      uniqueEdges.add(edge.id)
-      currentID = edge.id.id
-
-      printIndentedLine(0, "Edge")
-      printIndentedLine(1, "id: " + edge.id.id)
-      printIndentedLine(1, "from: " + edge.src)
-      printIndentedLine(1, "to: " + edge.dst)
-    }
+    val identifier = "edge-" + edge.id.id
+    printIndentedLine(identifier, 0, "Edge")
+    printIndentedLine(identifier, 1, "id: " + edge.id.id)
+    printIndentedLine(identifier, 1, "from: " + edge.src.id)
+    printIndentedLine(identifier, 1, "to: " + edge.dst.id)
   }
-  def printIndentedLine(indent : Int, s : String = "") = {
-    println(currentID + (if (s == "") "" else " " + ("    " * indent) + s))
+  def printIndentedLine(identifier : String, indent : Int, s : String = "") = {
+    println(identifier + (if (s == "") "" else " " + ("    " * indent) + s))
   }
 }
