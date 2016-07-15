e5510e9937fca3526a77d346734928993ea6374f
Pierce Darragh <pierce.darragh@gmail.com>
2016-07-15 15:56:34 -0600

    Coverage improved (but messy)
    Coverage tool seems fully functional...
    
    ...for now. *ominous music*

--- src/tools/src/main/scala/org/ucombinator/jaam/tools/Coverage.scala
+++ src/tools/src/main/scala/org/ucombinator/jaam/tools/Coverage.scala
@@ -1,7 +1,7 @@
 package org.ucombinator.jaam.tools
 
 import java.io.{File, FileInputStream}
-import java.lang.reflect.Method
+import java.lang.reflect.{Constructor, Method}
 import java.net.{URL, URLClassLoader}
 import java.util.jar.JarFile
 
@@ -18,16 +18,69 @@ import scala.collection.mutable
 sealed trait OriginalMethod
 case class SootMethod(method: soot.SootMethod) extends OriginalMethod
 case class ReflectMethod(method: Method) extends OriginalMethod
+case class ReflectConstructor(constructor: Constructor[_]) extends OriginalMethod
+case class ClassStaticInitializer(callingClass: Class[_]) extends OriginalMethod
 
-class ComparableMethod(val representation: String, original: OriginalMethod) {
-  def this(reflectMethod: Method) = this(reflectMethod.toGenericString, ReflectMethod(reflectMethod))
-  def this(sootStmt: Stmt) = this(sootStmt.method.toString, SootMethod(sootStmt.method))
+class ComparableMethod(representation: String, val originalMethod: OriginalMethod, val jarFile: String, val classFile: String) {
+  def this(reflectMethod: Method, jarFile: String, classFile: String) = this(reflectMethod.toGenericString, ReflectMethod(reflectMethod), jarFile, classFile)
+  def this(reflectConstructor: Constructor[_], jarFile: String, classFile: String) = this(reflectConstructor.toGenericString, ReflectConstructor(reflectConstructor), jarFile, classFile)
+  def this(sootStmt: Stmt, jarFile: String = "", classFile: String = "") = this(sootStmt.method.toString, SootMethod(sootStmt.method), jarFile, classFile)
+  def this(loadedClass: Class[_]) = this(loadedClass.toString, ClassStaticInitializer(loadedClass), "", "")
 
   override def equals(other: Any): Boolean = {
     other match {
       case other: ComparableMethod =>
-        this.representation == other.representation
+        this.getRepresentation == other.getRepresentation
       case _ => false
+    }
+  }
+
+  override def hashCode: Int = {
+    this.getRepresentation.hashCode
+  }
+
+  def getRepresentation: String = {
+//    print("getting representation of ")
+    originalMethod match {
+      //      case SootMethod(s) => s.getName + " returns: " + s.getReturnType + " takes: " + s.getParameterTypes
+      //      case SootMethod(s) => s.getDeclaration
+      //      case ReflectMethod(r) => r.toGenericString + r.getClass.getName
+      case SootMethod(s)    =>
+//        println("SootMethod")
+        s.getDeclaringClass.toString.replace("$", ".") + " " + s.getReturnType.toString + " " + s.getName + "(" + s.getParameterTypes.map(t => t.toString).mkString(", ") + ")"
+      //      case ReflectMethod(r) => r.getReturnType.getName + " " + r.getDeclaringClass.getName + "." + r.getName + "(" + r.getParameterTypes.map(t => t.getName).mkString(", ")  + ")"
+      //      case ReflectMethod(r) => jarFile + " " + " " + classFile + " " + r.getReturnType.getName + " " + r.getDeclaringClass.getName + " " + r.getName + "(" + r.getParameterTypes.map(t => t.getName).mkString(", ")  + ")"
+      case ReflectMethod(r) =>
+//        println("ReflectMethod")
+//        r.getDeclaringClass.getCanonicalName.replace("$", ".") + " " + r.getReturnType.getCanonicalName.replace("$", ".") + " " + r.getName + "(" + r.getParameterTypes.map(t => t.getCanonicalName.replace("$", ".")).mkString(", ")  + ")"
+        r.getDeclaringClass.getName.replace("$", ".") + " " + r.getReturnType.getName.replace("$", ".") + " " + r.getName + "(" + r.getParameterTypes.map(t => t.getName.replace("$", ".")).mkString(", ")  + ")"
+//        "REFLECTMETHOD"
+      case ReflectConstructor(c) =>
+//        println("ReflectConstructor")
+//        println("constructor: " + c)
+//        println("constructor.getDeclaringClass: " + c.getDeclaringClass)
+//        println("constructor.getDeclaringClass.getCanonicalName: " + c.getDeclaringClass.getCanonicalName)
+//        println("constructor.getDeclaringClass.getName: " + c.getDeclaringClass.getName)
+//        println("constructor.getDeclaringClass.getSimpleName: " + c.getDeclaringClass.getSimpleName)
+//        println("constructor.getDeclaringClass.getCanonicalName.replace(blah): " + c.getDeclaringClass.getCanonicalName.replace("$", "."))
+//        println("constructor.parameters.getCanonicalName(s): " + c.getParameterTypes.map(t => t.getCanonicalName.replace("$", ".")).mkString(", "))
+        c.getDeclaringClass.getName.replace("$", ".") + " " + "void" + " " + "<init>" + "(" + c.getParameterTypes.map(t => t.getName.replace("$", ".")).mkString(", ") + ")"
+      case ClassStaticInitializer(i) =>
+        i.getName.replace("$", ".") + " " + "void <clinit>()"
+    }
+  }
+
+  override def toString: String = {
+    originalMethod match {
+      //      case SootMethod(s) => s.getName + " returns: " + s.getReturnType + " takes: " + s.getParameterTypes
+      //      case SootMethod(s) => s.getDeclaration
+      //      case ReflectMethod(r) => r.toGenericString + r.getClass.getName
+      case SootMethod(s)    => s.getReturnType         + " " + s.getDeclaringClass.getName + "." + s.getName + "(" + s.getParameterTypes.map(t => t.toString).mkString(", ") + ")"
+      //      case ReflectMethod(r) => r.getReturnType.getName + " " + r.getDeclaringClass.getName + "." + r.getName + "(" + r.getParameterTypes.map(t => t.getName).mkString(", ")  + ")"
+      //      case ReflectMethod(r) => jarFile + " " + " " + classFile + " " + r.getReturnType.getName + " " + r.getDeclaringClass.getName + " " + r.getName + "(" + r.getParameterTypes.map(t => t.getName).mkString(", ")  + ")"
+      case ReflectMethod(r) => r.getReturnType.getCanonicalName + " " + r.getName + "(" + r.getParameterTypes.map(t => t.getCanonicalName).mkString(", ")  + ")"
+      case ReflectConstructor(c) => c.getName + "(" + c.getParameterTypes.map(t => t.getCanonicalName).mkString(", ") + ")"
+      case ClassStaticInitializer(i) => this.getRepresentation
     }
   }
 }
@@ -44,6 +97,7 @@ object Coverage {
   def findCoverage(jaamFile: String, jarFileNames: Seq[String], additionalJarFileNames: Seq[String]) = {
     initializeHashFromJarFiles(jarFileNames, additionalJarFileNames)
     loadStmtsFromJaamFile(jaamFile)
+    compareMethods()
     // Iterate over statements in jaam file.
     //   For each statement, add statement to method in hash.
     // Print results.
@@ -55,7 +109,7 @@ object Coverage {
     val additionalJarFileURLs = additionalJarFileNames.map(name => new File(name).toURI.toURL).toArray
     val classLoader = new URLClassLoader(additionalJarFileURLs ++ jarFileURLs)
     for (jarFileName <- jarFileNames) {
-      println(jarFileName)
+//      println("jarFileName: " + jarFileName)
       // Initialize class loader.  Unfortunately, this only works if you know
       // the names of the classes to load... which is unhelpful to us.
       val jarFile = new JarFile(jarFileName, false)
@@ -64,19 +118,30 @@ object Coverage {
         // e.g. "com.example.MyGreatClass".
         val fileName = jarEntry.getName
         val binaryName = fileName.dropRight(classEnding.length).replaceAll(File.separator, ".")
-        println("    " + binaryName)
+//        println("  classname: " + fileName)
         try {
           val loadedClass = classLoader.loadClass(binaryName)
-          loadedClass.getMethods.foreach(m => methodMap(new ComparableMethod(m)) = StmtSet())
+//          println("    declaredMethods: " + loadedClass.getDeclaredMethods.map(m => m.toGenericString).toList)
+//          println("    declaredConstructors: " + loadedClass.getDeclaredConstructors.map(m => m.toGenericString).toList)
+//          loadedClass.getDeclaredMethods.foreach(m => {val cm = new ComparableMethod(m, jarFileName, fileName); methodMap(cm) = StmtSet(); println("      " + cm.getRepresentation)})
+//          loadedClass.getDeclaredConstructors.foreach(m => {val cm = new ComparableMethod(m, jarFileName, fileName); methodMap(cm) = StmtSet(); println("      " + cm.getRepresentation)})
+          methodMap(new ComparableMethod(loadedClass)) = StmtSet()
+          loadedClass.getDeclaredMethods.foreach(m => methodMap(new ComparableMethod(m, jarFileName, fileName)) = StmtSet())
+          loadedClass.getDeclaredConstructors.foreach(m => methodMap(new ComparableMethod(m, jarFileName, fileName)) = StmtSet())
         } catch {
           //TODO: Decide whether to catch these exceptions explicitly.
           // Skip NoClassDefFoundErrors because they do not concern us.
 //          case e: NoClassDefFoundError => ()
           // Loading of classes does not always seem to succeed. Unsure why.
-          case e: Throwable => println("        " + e)
+          case e: Throwable => e.printStackTrace()
         }
       }
     }
+//    for ((key, value) <- methodMap) {
+//      println(key.getRepresentation)
+//    }
+//    println("*******")
+//    println("methodMap.size: " + methodMap.size)
   }
 
   def loadStmtsFromJaamFile(jaamFile: String) = {
@@ -87,14 +152,48 @@ object Coverage {
       packet match {
         case s: State =>
           val comparable = new ComparableMethod(s.stmt)
+//          println(comparable.getRepresentation)
           methodMap get comparable match {
             case Some(_) => methodMap(comparable) += s.stmt
-            case None => methodMap(comparable) = StmtSet()
+            case None =>
+//              println(comparable.getRepresentation)
+//              println(comparable.toString)
           }
         case _ => () // Ignore anything else.
       }
     }
     pi.close()
+  }
+
+  def compareMethods() = {
+    var count = 0
+    for ((method, stmtSet) <- methodMap) {
+      if (stmtSet.nonEmpty) {
+        method.originalMethod match {
+          case SootMethod(s) =>
+//            println("JAAM-initialized method:")
+//            println("    " + method.getRepresentation)
+          case ReflectMethod(r) =>
+//            println("JAR-initialized method:")
+//            println("    " + method.getRepresentation)
+          case _ => ()
+        }
+//        println(method.getRepresentation)
+      } else {
+        count += 1
+        method.originalMethod match {
+          case ReflectMethod(r) =>
+//            println("Empty JAR-initialized method:")
+//            println("    " + method.getRepresentation)
+          case SootMethod(s) =>
+//            println("Empty JAAM-initialized method:")
+//            println("    " + method.getRepresentation)
+          case _ => ()
+        }
+      }
+//      println(method.getRepresentation)
+    }
+    println("Missing " + count + " of " + methodMap.size)
   }
 }
 
