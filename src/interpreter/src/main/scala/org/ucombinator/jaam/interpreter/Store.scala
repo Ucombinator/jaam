df0fa6f3aab4272c1319d0efb2b52da9e05a247b
Guannan Wei <kiss.kraks@gmail.com>
2016-12-15 15:48:20 -0700

    fix update in Store

--- src/interpreter/src/main/scala/org/ucombinator/jaam/interpreter/Store.scala
+++ src/interpreter/src/main/scala/org/ucombinator/jaam/interpreter/Store.scala
@@ -76,6 +76,7 @@ abstract class AbstractStore[K <: Addr, V](val map: mutable.Map[K, V], abstractD
   def strongUpdate(addr: K, d: V, modified: Boolean): AbstractStore[K, V] = {
     if (modified) {
       if (on) writeAddrs += addr
+      readAddrs += addr
       map += (addr -> d)
     }
     this
