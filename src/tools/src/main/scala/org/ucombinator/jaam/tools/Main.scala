8512c4d2a3ccada726e808af7178119cabc30253
Pierce Darragh <pierce.darragh@gmail.com>
2016-12-19 12:48:19 -0700

    Fixed call for Coverage2
    Now properly handles filenames and whatnot.

--- src/tools/src/main/scala/org/ucombinator/jaam/tools/Main.scala
+++ src/tools/src/main/scala/org/ucombinator/jaam/tools/Main.scala
@@ -21,6 +21,7 @@ object Conf {
   class Validate extends Main("validate") {
     banner("Amend an aborted JAAM serialization to allow reading.")
     footer("")
+
     val fixEof = opt[Boolean](descr = "whether to amend a JAAM file that ends abruptly")
     val addMissingStates = opt[Boolean](descr = "find hanging edges and add MissingState states so they go somewhere")
     val removeMissingStates = opt[Boolean](descr = "remove any MissingState states found in the serialization; overrides --addMissingStates")
@@ -38,6 +39,7 @@ object Conf {
   class Info extends Main("info") {
     banner("Get simple information about a JAAM interpretation.")
     footer("")
+
     val file = trailArg[java.io.File](descr = "a .jaam file to be analyzed")
 
     def run(conf: Conf) {
@@ -48,6 +50,7 @@ object Conf {
   class Cat extends Main("cat") {
     banner("Combine multile JAAM files into a single, cohesive file.")
     footer("")
+
     val outFile = trailArg[java.io.File](descr = "The desired output filename")
     val inFiles = trailArg[List[String]](descr = "The list of files to be concatenated.")
 
@@ -59,6 +62,7 @@ object Conf {
   class Coverage extends Main("coverage") {
     banner("Analyze a JAAM file against target JAR files to find JAAM coverage.")
     footer("")
+
     val jaamFile = trailArg[java.io.File](descr = "The JAAM file to analyze")
     val jars = trailArg[String](descr = "Colon-separated list of JAR files to directly compare coverage against")
     val additionalJars = opt[String](descr = "Colon-separated list of JAR files to complete class loading for inspection JAR files")
@@ -71,21 +75,27 @@ object Conf {
   class Coverage2 extends Main("coverage2") {
     banner("Analyze a JAAM file against target JAR files to find JAAM coverage.")
     footer("")
+
     val rtJar = trailArg[String](descr = "The RT.jar file to use for analysis")
     val jaamFile = trailArg[java.io.File](descr = "The JAAM file to analyze")
     val mainClass = trailArg[String](descr = "The name of the main class in the JAAM file")
     val jars = trailArg[String](descr = "Colon separated list of JAR files to directly compare coverage against")
     val additionalJars = opt[String](descr = "Colon-separated list of JAR files to complete class loading for inspection JAR files")
-    //val additionalJars = trailArg[String](descr = "Colon separated list of JAR files to complete class loading for inspection JAR files")
 
     def run(conf: Conf) {
-      Coverage2.main(rtJar.toString(), jaamFile().toString, mainClass.toString(), jars().split(":"), additionalJars.toString().split(":"))
+      val s_additionalJars: Seq[String] = additionalJars.toOption match {
+        case Some(s) => s.split(":")
+        case None => Seq[String]()
+      }
+
+      Coverage2.main(rtJar(), jaamFile().toString, mainClass(), jars().split(":"), s_additionalJars)
     }
   }
 
   class MissingReturns extends Main("missing-returns") {
     banner("Find calls with no matching return")
     footer("")
+
     val jaamFile = trailArg[java.io.File](descr = "The JAAM file to analyze")
 
     def run(conf: Conf) {
@@ -95,7 +105,7 @@ object Conf {
 }
 
 class Conf(args : Seq[String]) extends ScallopConf(args = args) {
-  banner("Usage: jaam-tools [options]")
+  banner("Usage: jaam-tools [subcommand] [options]")
   addSubcommand(new Conf.Print)
   addSubcommand(new Conf.Validate)
   addSubcommand(new Conf.Info)
