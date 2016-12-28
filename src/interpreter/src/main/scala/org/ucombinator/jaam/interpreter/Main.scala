7d3c5a25108a741376a9021ca6b3fdd3028a57c7
Michael D. Adams <https://michaeldadams.org/>
2016-12-27 19:37:27 -0700

    Added TODO item

--- src/interpreter/src/main/scala/org/ucombinator/jaam/interpreter/Main.scala
+++ src/interpreter/src/main/scala/org/ucombinator/jaam/interpreter/Main.scala
@@ -1138,6 +1138,7 @@ object System {
   */
 class Conf(args : Seq[String]) extends JaamConf(args = args) {
   // TODO: banner("")
+  // TODO: how do we turn off sorting of options by name?
 
   val classpath   = opt[String](required = true, short = 'P', descr = "the TODO class directory")
   val rtJar       = opt[String](required = true, short = 'J', descr = "the rt.jar file")
