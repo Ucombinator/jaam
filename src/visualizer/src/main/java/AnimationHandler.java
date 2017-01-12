18b7186115de00abe3d32285a16944ff7d69d05a
Timothy Johnson <timjohnson314@gmail.com>
2017-01-12 00:30:50 -0800

    Restored AnimationHandler

--- src/visualizer/src/main/java/AnimationHandler.java
+++ src/visualizer/src/main/java/AnimationHandler.java
@@ -1,24 +1,21 @@
-	import java.awt.Point;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Observable;
 
 import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
-import javafx.collections.ObservableList;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
-import javafx.geometry.Point2D;
-import javafx.scene.Group;
+
 import javafx.scene.Node;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+
 import javafx.util.Duration;
 
+// TODO: Use this example to create custom resizing animations:
+// https://rterp.wordpress.com/2015/09/01/creating-custom-animated-transitions-with-javafx/
 public class AnimationHandler implements javafx.event.EventHandler<javafx.scene.input.MouseEvent>
 {
 	@Override
@@ -49,242 +46,138 @@ public class AnimationHandler implements javafx.event.EventHandler<javafx.scene.
 		}
 	}
 
-	private void handlePrimaryDoubleClickTim(MouseEvent event)
+	private void collapse(final AbstractVertex v)
 	{
-		AbstractVertex v = ((GUINode)(event.getSource())).getVertex();
-		if(v.isExpanded())
+		for(final Node n : v.getGraphics().getChildren())
 		{
-			System.out.println("Collapsing node: " + v.id);
-			// Move center up from half of current height to half of new height
-			TranslateTransition tt = new TranslateTransition(Duration.millis(300), v.getGraphics());
-			tt.setByX(Parameters.stFrame.mainPanel.scaleX((AbstractVertex.DEFAULT_WIDTH - v.getWidth()) / 2.0));
-			tt.setByY(Parameters.stFrame.mainPanel.scaleY(-v.getHeight()/2.0 + AbstractVertex.DEFAULT_HEIGHT/2.0 - LayoutAlgorithm.MARGIN_PADDING));
-			tt.play();
-
-			// Scale to new height and width
-			ScaleTransition st = new ScaleTransition(Duration.millis(300), v.getGraphics());
-			st.setToX(AbstractVertex.DEFAULT_WIDTH / v.getWidth());
-			st.setToY(AbstractVertex.DEFAULT_HEIGHT / v.getHeight());
-			st.play();
-
-			v.setExpanded(false);
-
-			LayoutAlgorithm.layout(Parameters.stFrame.mainPanel.getPanelRoot());
-			st.setOnFinished(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event)
-				{
-					Iterator<AbstractVertex> itMethodVertices = Parameters.stFrame.mainPanel.getPanelRoot().getInnerGraph().getVertices().values().iterator();
-
-					while (itMethodVertices.hasNext())
-					{
-						AbstractVertex vertex = itMethodVertices.next();
-						animate(vertex);
-					}
-				}
-			});
-		}
-		else
-		{
-			// Expand vertex
-			System.out.println("Expanding node: " + v.id);
-			ScaleTransition st = new ScaleTransition(Duration.millis(300), v.getGraphics());
-			st.setToX(AbstractVertex.DEFAULT_WIDTH);
-			st.setToY(AbstractVertex.DEFAULT_HEIGHT);
-			st.play();
-
-			v.setExpanded(true);
-			LayoutAlgorithm.layout(Parameters.stFrame.mainPanel.getPanelRoot());
-			st.setOnFinished(new EventHandler<ActionEvent>()
-			{
-				@Override
-				public void handle(ActionEvent event)
-				{
-					Iterator<AbstractVertex> itMethodVertices = Parameters.stFrame.mainPanel.getPanelRoot().getInnerGraph().getVertices().values().iterator();
-
-					while (itMethodVertices.hasNext())
-					{
-						AbstractVertex vertex = itMethodVertices.next();
-						animate(vertex);
-					}
-				}
-			});
-		}
-		event.consume();
-	}
-	
-	private void collapsing(MouseEvent event)
-	{
-		AbstractVertex v = ((GUINode)(event.getSource())).getVertex();
-		
-		Iterator<Node> it = v.getGraphics().getChildren().iterator();
-		while(it.hasNext())
-		{
-			Node n = it.next();
 			if(!n.getClass().equals(Rectangle.class))
 			{
 				FadeTransition ft = new FadeTransition(Duration.millis(300), n);
 				ft.setToValue(0.0);
-				
+
 				ft.setOnFinished(new EventHandler<ActionEvent>() {
 					@Override
-					public void handle(ActionEvent event) {		
+					public void handle(ActionEvent event) {
 						n.setVisible(false);
 					}
 				});
-				
+
 				ft.play();
-				event.consume();
 			}
 		}
-		
+
 		TranslateTransition tt = new TranslateTransition(Duration.millis(300), v.getGraphics());
 		tt.setByY(Parameters.stFrame.mainPanel.scaleY(-v.getHeight()/2.0 + AbstractVertex.DEFAULT_HEIGHT/2.0));
 		tt.play();
-		
-		
-		ScaleTransition st = new ScaleTransition(Duration.millis(300), v.getGraphics());
 
-		//st.setFromX(Parameters.stFrame.mainPanel.scaleX(1.0));
-		st.setByX(AbstractVertex.DEFAULT_WIDTH/v.getWidth() -1 );
-		//st.setFromY(Parameters.stFrame.mainPanel.scaleY(1.0));
-		st.setByY(AbstractVertex.DEFAULT_HEIGHT/v.getHeight() -1);
+		ScaleTransition st = new ScaleTransition(Duration.millis(300), v.getGraphics());
+		st.setByX(AbstractVertex.DEFAULT_WIDTH/v.getWidth() - 1);
+		st.setByY(AbstractVertex.DEFAULT_HEIGHT/v.getHeight() - 1);
 		st.play();
 
-		System.out.println("BEFORE");
-		Parameters.stFrame.mainPanel.getPanelRoot().getInnerGraph().printCoordinates();
-		
-		HashMap<String, Point2D> oldPositions = new HashMap<>();
-		savePositions(Parameters.stFrame.mainPanel.getPanelRoot(), oldPositions);
-		oldPositions.put(v.getStrID(), new Point2D(Parameters.stFrame.mainPanel.scaleX(v.getX()+LayoutAlgorithm.MARGIN_PADDING), Parameters.stFrame.mainPanel.scaleY(v.getY()-v.getHeight()/2.0 + AbstractVertex.DEFAULT_HEIGHT/2.0+LayoutAlgorithm.MARGIN_PADDING)));
-		
-		/*****************************************************/
 		v.setExpanded(false);
-		System.out.println("Coordinated of the double-clicked vertex:");
-		v.printCoordinates();
-		LayoutAlgorithm.layout(Parameters.stFrame.mainPanel.getPanelRoot());
-		v.printCoordinates();
-		/*****************************************************/
-		
-		System.out.println("AFTER");
-		Parameters.stFrame.mainPanel.getPanelRoot().getInnerGraph().printCoordinates();
-		
+		final AbstractVertex panelRoot = Parameters.stFrame.mainPanel.getPanelRoot();
+		LayoutAlgorithm.layout(panelRoot);
+
 		st.setOnFinished(new EventHandler<ActionEvent>() {
 			@Override
-			public void handle(ActionEvent event) {		
-				Iterator<AbstractVertex> itMethodVertices = Parameters.stFrame.mainPanel.getPanelRoot().getInnerGraph().getVertices().values().iterator();
-				
-				while(itMethodVertices.hasNext()){
-					AbstractVertex vertex = itMethodVertices.next();
+			public void handle(ActionEvent event)
+			{
+				for(AbstractVertex vertex : panelRoot.getInnerGraph().getVertices().values())
 					animate(vertex);
-					}			
-				}
-		});
 
+				panelRoot.recomputeGraphicsSize();
+				Edge.redrawEdges(v);
+			}
+		});
 	}
-	
-	private void expansing(MouseEvent event)
+
+	private void expand(final AbstractVertex v)
 	{
-		AbstractVertex v = ((GUINode)(event.getSource())).getVertex();
-		
-		Iterator<Node> it = v.getGraphics().getChildren().iterator();
-		while(it.hasNext())
+		for(final Node n: v.getGraphics().getChildren())
 		{
-			Node n = it.next();
 			if(!n.getClass().equals(Rectangle.class))
 			{
 				FadeTransition ft = new FadeTransition(Duration.millis(300), n);
 				ft.setToValue(1);
-				
+
 				ft.setOnFinished(new EventHandler<ActionEvent>() {
 					@Override
-					public void handle(ActionEvent event) {		
+					public void handle(ActionEvent event) {
 						n.setVisible(true);
 					}
 				});
-				
+
 				ft.play();
-				event.consume();
 			}
 		}
-		
-		
-					System.out.println("Expanding node: " + v.id);
-					ScaleTransition st = new ScaleTransition(Duration.millis(300), v.getGraphics());
-					st.setToX(AbstractVertex.DEFAULT_WIDTH);
-					st.setToY(AbstractVertex.DEFAULT_HEIGHT);
-					st.play();
 
-					v.setExpanded(true);
-					LayoutAlgorithm.layout(Parameters.stFrame.mainPanel.getPanelRoot());
-					st.setOnFinished(new EventHandler<ActionEvent>()
-					{
-						@Override
-						public void handle(ActionEvent event)
-						{
-							Iterator<AbstractVertex> itMethodVertices = Parameters.stFrame.mainPanel.getPanelRoot().getInnerGraph().getVertices().values().iterator();
+		v.setExpanded(true);
+		final AbstractVertex panelRoot = Parameters.stFrame.mainPanel.getPanelRoot();
+		LayoutAlgorithm.layout(panelRoot);
 
-							while (itMethodVertices.hasNext())
-							{
-								AbstractVertex vertex = itMethodVertices.next();
-								animate(vertex);
-							}
-						}
-					});
-					
+		ScaleTransition st = new ScaleTransition(Duration.millis(300), v.getGraphics());
+		st.setToX(AbstractVertex.DEFAULT_WIDTH);
+		st.setToY(AbstractVertex.DEFAULT_HEIGHT);
+		st.play();
 
+		st.setOnFinished(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				for(AbstractVertex vertex : panelRoot.getInnerGraph().getVertices().values())
+					animate(vertex);
 
+				panelRoot.recomputeGraphicsSize();
+				Edge.redrawEdges(v);
+			}
+		});
 	}
-	
+
 	private void handlePrimaryDoubleClick(MouseEvent event)
 	{
-		if((((GUINode)(event.getSource())).getVertex()).isExpanded()){
-			collapsing(event);
-		}else{
-			expansing(event);
-		}
-				
 		event.consume();
-	}
-
-	
-	private void savePositions(AbstractVertex v, HashMap<String, Point2D> oldPositions){
-		
-		
-		oldPositions.put(v.getStrID(), 
-				new Point2D(Parameters.stFrame.mainPanel.scaleX(v.getX()), 
-							Parameters.stFrame.mainPanel.scaleY(v.getY())));
-		
-		Iterator<AbstractVertex> it = v.getInnerGraph().getVertices().values().iterator();
-		while (it.hasNext())
-		{
-			savePositions(it.next(),oldPositions);
-		}
+		AbstractVertex v = ((GUINode)(event.getSource())).getVertex();
+		if(v.isExpanded())
+			collapse(v);
+		else
+			expand(v);
 	}
 
 	public void animate(AbstractVertex v)
 	{
-		
-//		v.getGraphics().setLayoutX(Parameters.stFrame.mainPanel.scaleX(v.getX()));
-//		v.getGraphics().setLayoutY(Parameters.stFrame.mainPanel.scaleY(v.getY()));
+		// Because vertices scale around their center, we have to shift them to keep the top left corner in the correct place.
+		double xShift = v.getGraphics().getXShift();
+		double yShift = v.getGraphics().getYShift();
 		TranslateTransition tt = new TranslateTransition(Duration.millis(300), v.getGraphics());
-		double oldWidth = v.getGraphics().getWidth();
-		double currentWidth = v.getGraphics().getScaleX()*oldWidth;
-		double oldHeight = v.getGraphics().getHeight();
-		double currentHeight = v.getGraphics().getScaleY()*oldHeight;
-		
-//		tt.setToX(Parameters.stFrame.mainPanel.scaleX(v.getX()) - (oldWidth-currentWidth)/2);
-//		tt.setToY(Parameters.stFrame.mainPanel.scaleY(v.getY()) - (oldHeight-currentHeight)/2);
-		tt.setToX(Parameters.stFrame.mainPanel.scaleX(v.getX()) - (oldWidth-currentWidth)/2);
-		tt.setToY(Parameters.stFrame.mainPanel.scaleY(v.getY()) - (oldHeight-currentHeight)/2);
+		tt.setToX(Parameters.stFrame.mainPanel.scaleX(v.getX()) - xShift);
+		tt.setToY(Parameters.stFrame.mainPanel.scaleY(v.getY()) - yShift);
 		tt.play();
-
 	}
 
 	private void handlePrimarySingleClick(MouseEvent event)
 	{
-		AbstractVertex v = ((GUINode)(event.getSource())).getVertex();
-		System.out.println("Single click: " + v.getStrID());
 		event.consume();
+		AbstractVertex v = ((GUINode)(event.getSource())).getVertex();
+
+		v.toggleSelected();
+		// TODO: Find better ways to highlight/unhighlight
+		if(v.isSelected())
+			v.getGraphics().increaseOpacity();
+		else
+			v.getGraphics().decreaseOpacity();
+
+		// If we are in debug mode, the bytecode area does not exist.
+		if(Parameters.bytecodeArea != null) {
+			if (v.isSelected())
+				Parameters.bytecodeArea.setVertex(v);
+			else
+				Parameters.bytecodeArea.clear();
+
+			Parameters.bytecodeArea.setDescription();
+			// The repaint seems to happen automatically? I'm not sure why.
+			//Parameters.repaintAll();
+		}
 	}
 }
