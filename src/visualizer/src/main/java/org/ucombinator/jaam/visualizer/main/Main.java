81e2f1bdc1ab0904ec2d52bd669494f1189e8d04
Timothy Johnson <timjohnson314@gmail.com>
2017-04-25 15:50:49 -0700

    Commented out loading dummy graph

--- src/visualizer/src/main/java/org/ucombinator/jaam/visualizer/main/Main.java
+++ src/visualizer/src/main/java/org/ucombinator/jaam/visualizer/main/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application
 		stage.show();
 
 		// Read dummy graph
-		outerFrame.loadGraph(false);
+		//outerFrame.loadGraph(false);
 
 		/*for (int i = 0; i < args.length; i++)
 		{
