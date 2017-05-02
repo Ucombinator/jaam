aa89a6dca327403aed4d6300713476517030042a
Giordano <giordano@Giordanos-MacBook-Pro.local>
2017-05-01 23:28:17 -0600

    debug mode off

--- src/visualizer/src/main/java/org/ucombinator/jaam/visualizer/main/Main.java
+++ src/visualizer/src/main/java/org/ucombinator/jaam/visualizer/main/Main.java
@@ -15,7 +15,7 @@ import org.ucombinator.jaam.visualizer.gui.OuterFrame;
 public class Main extends Application
 {
 	
-	public static final boolean debug = true; 
+	public static final boolean debug = false; 
 	
 	private static OuterFrame outerFrame;
 
