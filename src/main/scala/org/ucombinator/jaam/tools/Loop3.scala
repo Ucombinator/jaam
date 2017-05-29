60803201e0ccc9e50131913f92cd9881b6c9b1bc
Michael D. Adams <https://michaeldadams.org/>
2017-05-29 13:23:05 -0600

    Make loop3 list all statements

--- src/main/scala/org/ucombinator/jaam/tools/Loop3.scala
+++ src/main/scala/org/ucombinator/jaam/tools/Loop3.scala
@@ -1,9 +1,10 @@
 package org.ucombinator.jaam.tools.loop3
 
 import org.ucombinator.jaam.tools
-import soot.PackManager
+import soot.{ PackManager, Scene, SootClass, SootMethod, SootResolver }
 import soot.options.Options
 
+import scala.collection.JavaConverters._
 
 object Loop3 extends tools.Main("loop3") {
   val classpath = opt[List[String]](descr = "TODO")
@@ -36,8 +37,84 @@ object Main {
     //Scene.v.setSootClassPath(classpath)
     //Scene.v.loadNecessaryClasses
 
-    PackManager.v.runPacks
+    Scene.v.loadBasicClasses()
+    PackManager.v.runPacks()
 
-    println("END")
+    def getSootClass(s : String) = Scene.v().loadClass(s, SootClass.SIGNATURES)
+    def getBody(m : SootMethod) = {
+      if (m.isNative) { throw new Exception("Attempt to Soot.getBody on native method: " + m) }
+      if (m.isAbstract) { throw new Exception("Attempt to Soot.getBody on abstract method: " + m) }
+      // TODO: do we need to test for phantom here?
+      if (!m.hasActiveBody()) {
+        SootResolver.v().resolveClass(m.getDeclaringClass.getName, SootClass.BODIES)
+        m.retrieveActiveBody()
+      }
+      m.getActiveBody
+    }
+
+    //println("dc" + Scene.v.dynamicClasses().asScala)
+    println("ap" + Scene.v.getApplicationClasses().asScala)
+    println("al" + Scene.v.getClasses().asScala)
+    println("dan" + Scene.v.getClasses(SootClass.DANGLING).asScala)
+    println("lib" + Scene.v.getLibraryClasses().asScala)
+    println("phan" + Scene.v.getPhantomClasses().asScala)
+
+    val c = getSootClass("java.lang.Object")
+    println("hier " + Scene.v.getActiveHierarchy())
+    println("hier sub " + Scene.v.getActiveHierarchy().getSubclassesOf(c))
+    println("fast hier " + Scene.v.getOrMakeFastHierarchy())
+    println("hier sub " + Scene.v.getFastHierarchy().getSubclassesOf(c))
+    println("entry " + Scene.v.getEntryPoints().asScala)
+    //println("main " + Scene.v.getMainClass())
+    println("pkg " + Scene.v.getPkgList())
+
+
+//    val c = getSootClass("edu.cyberapex.home.StacMain")
+//    println("Class: " + c)
+//    for (m <- c.getMethods.asScala) {
+//      println("Method: " + m)
+//      for (s <- getBody(m).getUnits.asScala) {
+//        println("Stmt: " + s)
+//      }
+//    }
+
+    var class_count = 0
+    var method_count = 0
+    var stmt_count = 0
+
+    for (p <- classpath) {
+      println(f"p $p")
+      val jar = new java.util.jar.JarInputStream(
+        new java.io.FileInputStream(p))
+
+      var entry: java.util.jar.JarEntry = null
+      while ({entry = jar.getNextJarEntry(); entry != null}) {
+        class_count += 1
+        val name = entry.getName.replace("/", ".").replaceAll("\\.class$", "")
+        println(f"class $class_count: $name")
+
+        val c = getSootClass(name)
+        for (m <- c.getMethods.asScala) {
+          method_count += 1
+          println(f"method $method_count: $m")
+          if (m.isNative) { println("skipping body because native") }
+          else if (m.isAbstract) { println("skipping body because abstract") }
+          else {
+            for (s <- getBody(m).getUnits.asScala) {
+              stmt_count += 1
+              println(f"stmt $stmt_count: $s")
+            }
+          }
+        }
+        //val bytes = new Array[Byte](entry.getSize.toInt)
+        //jar.read(bytes, 0, entry.getSize.toInt)
+        //typeLoader.add(bytes)
+      }
+    }
+
+    println(f"END classes=$class_count methods=$method_count stmts=$stmt_count")
   }
 }
+
+
+// ./bin/jaam-tools loop3 --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/airplan_1.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/commons-cli-1.3.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/commons-codec-1.9.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/commons-fileupload-1.3.1.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/commons-io-2.2.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/commons-lang3-3.4.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/commons-logging-1.2.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/httpclient-4.5.1.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/httpcore-4.4.3.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/jline-2.8.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/log4j-1.2.17.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/mapdb-2.0-beta8.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/netty-all-4.0.34.Final.jar --classpath ../../engagements/en4/article/stac_engagement_4_release_v1.1/challenge_programs/airplan_1/challenge_program/lib/protobuf-java-3.0.0-beta-2.jar --classpath resources/rt.jar
