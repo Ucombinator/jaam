35f0efc26359eb3f6656b4a142556806e3c4a62c
Juan Besa <jjbesavi@uci.edu>
2017-06-20 16:15:28 -0700

    Added two scripts to creates Jaam Files

---
+++ createJaamNoPruneNoShrink.sh
@@ -0,0 +1,9 @@
+file=`basename $1 ".jar"`
+./bin/jaam app --input $1  --output $file.app.jaam
+./bin/jaam decompile --input $file.app.jaam --output $file.decompile.jaam
+./bin/jaam loop3 --no-prune --no-shrink --input $file.app.jaam --output $file.loop3.jaam
+./bin/jaam cat --input $file.app.jaam --input $file.decompile.jaam --input $file.loop3.jaam --output $file.nPnS.all.jaam
+rm *.app.jaam
+rm *.loop3.jaam
+rm *.decompile.jaam
+#sh buildjaam.sh airplan_1
