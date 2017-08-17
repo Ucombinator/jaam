dd0423bb8c84fafd49a6ab1f0975971e2b264ef2
Michael D. Adams <https://michaeldadams.org/>
2017-08-17 03:56:25 -0600

    Moved `TakeInput` into `MainPaneController`

--- src/main/java/org/ucombinator/jaam/visualizer/controllers/MainPaneController.java
+++ src/main/java/org/ucombinator/jaam/visualizer/controllers/MainPaneController.java
@@ -6,12 +6,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import org.ucombinator.jaam.serializer.*;
 import org.ucombinator.jaam.visualizer.graph.Graph;
+import org.ucombinator.jaam.visualizer.layout.LayoutLoopVertex;
+import org.ucombinator.jaam.visualizer.layout.LayoutMethodVertex;
 import org.ucombinator.jaam.visualizer.main.Main;
-import org.ucombinator.jaam.visualizer.main.TakeInput;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainPaneController {
     @FXML private AnchorPane root;
@@ -26,7 +29,6 @@ public class MainPaneController {
 
     public void loadLoopGraph(ActionEvent event) throws IOException {
         Graph graph;
-        TakeInput ti = new TakeInput();
         String filename = "";
 
         System.out.println("Load graph: start...");
@@ -42,7 +44,7 @@ public class MainPaneController {
 
         String path = file.getAbsolutePath();
         System.out.println("File path: " + path);
-        graph = ti.parseLoopGraph(file.getAbsolutePath());
+        graph = parseLoopGraph(file.getAbsolutePath());
         filename = file.getName();
 
         System.out.println("--> Create visualization: start...");
@@ -56,6 +58,43 @@ public class MainPaneController {
         }
         tabPane.getTabs().add(tabController.tab);
         tabPane.getSelectionModel().select(tabController.tab);
+    }
+
+    public static Graph parseLoopGraph(String file) {
+        Graph graph = new Graph();
+
+        ArrayList<LoopEdge> edges = new ArrayList<>();
+        for (Packet packet : Serializer.readAll(file)) {
+            if (packet instanceof LoopLoopNode) {
+                LoopLoopNode node = (LoopLoopNode) packet;
+                graph.addVertex(new LayoutLoopVertex(node.id().id(),
+                        node.method().getSignature(),
+                        node.statementIndex()));
+            } else if (packet instanceof LoopMethodNode) {
+                LoopMethodNode node = (LoopMethodNode) packet;
+                graph.addVertex(new LayoutMethodVertex(node.id().id(),
+                        node.method().getSignature()));
+            } else if (packet instanceof LoopEdge) {
+                edges.add((LoopEdge) packet);
+            }
+//                else if (packet instanceof org.ucombinator.jaam.tools.decompile.DecompiledClass)
+//                {
+//                    CompilationUnit unit = ((org.ucombinator.jaam.tools.decompile.DecompiledClass) packet).compilationUnit();
+//                    String className = getClassName(unit);
+//                    graph.addClass(className, unit.getText());
+//                }
+
+//                System.out.println("Reading new packet...");
+//                packet = packetInput.read();
+//                System.out.println("Packet read!");
+        }
+
+        // We actually create the edges here
+        for (LoopEdge edge : edges) {
+            graph.addEdge(edge.src().id(), edge.dst().id());
+        }
+
+        return graph;
     }
 
     @FXML private void quit(ActionEvent event) {
